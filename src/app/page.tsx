c50ebd14677330a6050fa857e8278ed06d8f8e02
Felipe-MML <felipemaras20@gmail.com>
2025-08-21 11:37:30 -0300

    Adição de cores

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,3 @@
-"use client";
-
-import { getPrice } from "@/services/price.services";
-import { useState, useEffect } from "react";
-
 export default function Home() {
-  const [price, setPrice] = useState(null);
-
-  useEffect(() => {
-    getPrice("EUR", "BRL").then((data) => {
-      setPrice(data);
-    });
-  }, []);
-
-  console.log(price);
-
   return <div></div>;
 }
